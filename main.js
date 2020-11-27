833c1aad670015d693439d9d1b55b10cd5471397
fahadkhanpakistan <fahad.khan50269@gmail.com>
2020-11-27 22:51:10 +0500

    Minor bugs fixed!

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 const burger = document.getElementById("burger");
 const cross = document.getElementById("cross");
 const navList = document.getElementById("nav-list");
-
-
-
 burger.addEventListener("click", showNav);
-
 
 function showNav() {
     navList.classList.toggle("block");
     cross.classList.toggle("block");
     burger.classList.toggle("none");
     cross.addEventListener("click", showNav);
-
 
 }
